5524796e37a14c3fea4f36889f029aa2e85e0864
Bruno Augusto de Araujo Ortiz <brunortiz11@gmail.com>
2016-12-20 11:56:36 -0200

    ajustes

--- addons/Behave/Editor/Scripts/behavior_tree_editor.gd
+++ addons/Behave/Editor/Scripts/behavior_tree_editor.gd
@@ -7,6 +7,7 @@ onready var popup = get_node("TreeNodeSelector")
 func _ready():
 	connect("connection_request", self, "_on_connect_request")
 	connect("connection_to_empty", self, "_on_connect_to_empty")
+	connect("popup_request", self, "_on_popup_request")
 	popup.connect("tree_node_selected", self, "_on_node_selected")
 	
 func  _on_connect_request(from, from_slot, to, to_slot):
@@ -14,4 +15,8 @@ func  _on_connect_request(from, from_slot, to, to_slot):
 	
 func _on_connect_to_empty(from, from_slot, release_position):
 	popup.set_pos(release_position)
+	popup.popup()
+
+func _on_popup_request(p):
+	popup.set_pos(p)
 	popup.popup()
