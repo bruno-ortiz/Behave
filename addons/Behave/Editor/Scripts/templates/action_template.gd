4ac1c17687acd1c907de3ccd0ce811f95ac921da
Bruno Ortiz <bruno.ortiz@guiabolso.com.br>
2016-12-22 20:43:35 -0200

    blah

--- addons/Behave/Editor/Scripts/templates/action_template.gd
+++ addons/Behave/Editor/Scripts/templates/action_template.gd
@@ -3,8 +3,6 @@ var Status = preload("res://addons/Behave/Scripts/utils/status.gd")
 
 var status = Status.RUNNING
 
-var children = []
-
 func on_enter(context):
 	pass
 
--- addons/Behave/plugin.gd
+++ addons/Behave/plugin.gd
@@ -11,9 +11,6 @@ var script_editor
 func _enter_tree():
 	add_custom_type("BehaviorTree", "Node", preload("res://addons/Behave/Scripts/behavior_tree.gd"),  preload("res://addons/Behave/behv_root_icon.png"))
 	editor_instance = editor.instance()
-	
-	script_editor =find_script_editor()
-
 
 func _exit_tree():
 	remove_control_from_bottom_panel(editor_instance)
@@ -21,20 +18,11 @@ func _exit_tree():
 	queue_free()
 
 func handles(object):
+#edit_resource(load("res://addons/Behave/Editor/Scripts/utils.gd"))	
 	return object extends BehaviorTree
 
 func make_visible(visible):
 	if visible:
-		get_viewport().get_child(0).edit_resource(load("res://addons/Behave/Editor/Scripts/utils.gd"))
 		add_control_to_bottom_panel(editor_instance, "Behave")
 	else:
 		remove_control_from_bottom_panel(editor_instance)
-	
-
-func find_script_editor():
-	var editor = get_editor_viewport()
-	for i in ["ScriptEditor", "HSplitContainer", "TabContainer"]: editor = find_node(editor, i)
-	return editor
-	
-func find_node(node, type): # Node, String
-	if node != null: for i in node.get_children(): if i.get_type() == type: return i
