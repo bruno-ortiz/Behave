04f23c0cfc9f5e0c8385814e460cdeaf0c3d9fdf
Bruno Augusto de Araujo Ortiz <brunortiz11@gmail.com>
2017-02-07 23:48:12 -0200

    for some reason this change was not commited :/

---
+++ addons/Behave/Scripts/behavior_tree_node.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var Status = preload("res://addons/Behave/Scripts/utils/status.gd")
+var status = Status.NOT_STARTED
+
+func on_enter(context):
+	status = Status.RUNNING
+	context.tree.tickable_tasks.append(self)
+
+func tick(context):
+	pass
+
+func on_exit(context):
+	status = Status.TERMINATED
+	context.tree.tickable_tasks.remove(self)
